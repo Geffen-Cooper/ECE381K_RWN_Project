6fcde4aa14e09ce181bb710732babf5d18f4b0a0
Cooper Geffen (gc28692) <gc28692@ECE-A54980.austin.utexas.edu>
2022-10-08 13:54:08 -0500

    finished training function skeleton

--- src/train.py
+++ src/train.py
@@ -16,6 +16,10 @@ from dgl.nn import GraphConv
 import matplotlib.pyplot as plt
 import numpy as np
 from ogb.nodeproppred import DglNodePropPredDataset
+from torch.utils.tensorboard import SummaryWriter
+
+# our modules
+from models import GCN,GAT,GraphSage
 
 def train(args):
     print("training configuration:")
@@ -23,53 +27,114 @@ def train(args):
     print("Number of partitions:",args.k)
     print("Dataset:", args.dataset)
 
-    # first load the dataset
+    # first load the dataset, split into k partitions
     graph = load_dataset(args.dataset)
+    partitions = partition(graph,args.k)
 
-    # next load the model
-    model = load_model(args.gnn_model)
+    # init tensorboard
+    writer = SummaryWriter()
 
-    # create the optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    best_val_acc = 0
-    best_test_acc = 0
+    # training each partition
+    for idx,partition in enumerate(partitions):
+        # graph parameters
+        features = partition.ndata['feat']
+        labels = partition.ndata['label']
+        train_mask = partition.ndata['train_mask']
+        num_classes = partition.num_classes
 
-    features = g.ndata['feat']
-    labels = g.ndata['label']
-    train_mask = g.ndata['train_mask']
-    val_mask = g.ndata['val_mask']
-    test_mask = g.ndata['test_mask']
-    for e in range(100):
+        # create a gnn for this partition using graph parameters
+        model = load_model(args.gnn_model)
+
+        # create the optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        best_val_acc = 0
+
+        # train the subgraph
+        for e in range(100):
+            model.train()
+
+            # Forward
+            logits = model(partition, features)
+
+            # Compute prediction
+            pred = logits.argmax(1)
+
+            # Compute loss
+            # Note that you should only compute the losses of the nodes in the training set.
+            train_loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+            writer.add_scalar("Loss/train", train_loss, e)
+
+            # Compute accuracy on training dataset
+            train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+            writer.add_scalar("Accuracy/train", train_acc, e)
+
+            # evaluate on the validation set
+            val_acc, val_loss = validate(model, partition)
+            writer.add_scalar("Loss/val", val_loss, e)
+            writer.add_scalar("Accuracy/val", val_acc, e)
+
+            # Save the best validation accuracy and the corresponding model.
+            if best_val_acc < val_acc:
+                best_val_acc = val_acc
+                torch.save({
+                    'epoch': e+1,
+                    'model_state_dict': model.state_dict(),
+                    'val_acc': best_val_acc,
+                    }, 'best_model_' + str(args.dataset)+'_p'+str(idx+1)+'k'+str(len(graph))+'.pth') # e.g. best_model_cora_p1k5.pth is the best val accuracy for partition 1 of 5 gnn
+
+            # Backward
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+            # print the 
+            if e % 5 == 0:
+                print('In epoch {}, train loss: {:.3f}, train acc: {:.3f}, val loss: {:.3f}, val acc: {:.3f} (best val acc: {:.3f}))'.format(
+                    e, train_loss, train_acc, val_loss, val_acc, best_val_acc))
+    
+    # test the model
+    
+
+def validate(model, partition):
+    model.eval()
+
+    features = partition.ndata['feat']
+    labels = partition.ndata['label']
+    val_mask = partition.ndata['val_mask']
+
+    with torch.no_grad():
         # Forward
-        logits = model(g, features)
-
-        # Compute prediction
+        logits = model(partition, features)
         pred = logits.argmax(1)
 
-        # Compute loss
-        # Note that you should only compute the losses of the nodes in the training set.
-        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
-
-        # Compute accuracy on training/validation/test
-        train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+        # Compute loss and accuracy
+        val_loss = F.cross_entropy(logits[val_mask], labels[val_mask])
         val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
-        test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+        return val_acc
 
-        # Save the best validation accuracy and the corresponding test accuracy.
-        if best_val_acc < val_acc:
-            best_val_acc = val_acc
-            best_test_acc = test_acc
 
-        # Backward
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+# def test(model, graph):
+#     print("============ Testing Model ============")
+#     best_checkpoint = torch.load('best_model.pth')
+#     best_epoch = best_checkpoint['epoch']
+#     best_acc = best_checkpoint['val_acc']
+#     model.load_state_dict(best_checkpoint['state_dict'])
+#     model.eval()
+#     print("Best Val Acc: ", best_acc, " at epoch ", best_epoch)
 
-        # if e % 5 == 0:
-        #     print('In epoch {}, loss: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})'.format(
-        #         e, loss, val_acc, best_val_acc, test_acc, best_test_acc))
-    return best_test_acc
+#     features = graph.ndata['feat']
+#     labels = graph.ndata['label']
+#     test_mask = graph.ndata['val_mask']
 
+#     with torch.no_grad():
+#         # Forward
+#         logits = model(graph, features)
+#         pred = logits.argmax(1)
+
+#         # Compute loss and accuracy
+#         test_loss = F.cross_entropy(logits[test_mask], labels[test_mask])
+#         test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+#         return test_acc
 # ================================ datasets =====================================
 
 def load_dataset(dataset):
